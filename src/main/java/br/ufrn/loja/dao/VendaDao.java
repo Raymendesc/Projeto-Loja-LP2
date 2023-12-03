b297eaeb8a0722c1cd6cb381b5703fe09531d684
Douglas Felipe <117777160+DougFelipe@users.noreply.github.com>
2023-12-03 18:12:14 -0300

    FEAT: Template dao de vendas

---
+++ src/main/java/br/ufrn/loja/dao/VendaDao.java
@@ -0,0 +1,12 @@
+
+package br.ufrn.loja.dao;
+
+import br.ufrn.loja.model.Venda;
+
+/**
+ * Classe DAO para a entidade Venda.
+ */
+public class VendaDao {
+
+    // Métodos para operações de banco de dados (salvar, buscar, etc.)
+}
