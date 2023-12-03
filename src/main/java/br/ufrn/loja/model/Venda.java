4f24e6b10919d7cbadacae8ae26139cc6308fe0a
Douglas Felipe <117777160+DougFelipe@users.noreply.github.com>
2023-12-03 18:10:53 -0300

    FEAT: Template da função de vendas

---
+++ src/main/java/br/ufrn/loja/model/Venda.java
@@ -0,0 +1,17 @@
+
+package br.ufrn.loja.model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Classe que representa uma Venda.
+ */
+public class Venda {
+    private int id;
+    private List<Produto> produtos;
+    private Date dataVenda;
+    private double valorTotal;
+
+    // Construtor, getters e setters
+}
