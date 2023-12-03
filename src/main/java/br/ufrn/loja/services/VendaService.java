db35d0ba27b0555c4023b39f8e9a946979008541
Douglas Felipe <117777160+DougFelipe@users.noreply.github.com>
2023-12-03 18:12:57 -0300

    FEAT: Template services de vendas

---
+++ src/main/java/br/ufrn/loja/services/VendaService.java
@@ -0,0 +1,13 @@
+
+package br.ufrn.loja.services;
+
+import br.ufrn.loja.dao.VendaDao;
+import br.ufrn.loja.model.Venda;
+
+/**
+ * Classe de serviço para lógica de negócios relacionada a vendas.
+ */
+public class VendaService {
+
+    // Métodos para processar vendas, calcular total, verificar estoque, etc.
+}
